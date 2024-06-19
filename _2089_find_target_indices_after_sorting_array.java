8fa097fc09a1ceaf9836b3c0815a256669387df1
DEEP-op <samarrajpal25@gmail.com>
2024-06-19 18:17:32 +0530

    commiting 2089 solution

---
+++ _2089_find_target_indices_after_sorting_array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class _2089_find_target_indices_after_sorting_array {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter the length of the array :");
+    int n = s.nextInt();
+    int[] arr = new int[n];
+    for(int i =0; i<n; i++){
+      arr[i] = s.nextInt();
+    }
+    System.out.println("enter the target number :");
+    int target = s.nextInt();
+    System.out.println(Solution.targetIndices(arr,target));
+
+  }
+}
+
+class Solution {
+  public static List<Integer> targetIndices(int[] nums, int target) {
+      List<Integer> x = new ArrayList<Integer>();
+      Arrays.sort(nums);
+      for(int i = 0; i < nums.length; i++){
+          if(nums[i] == target){
+              x.add(i);
+          }
+      }
+      return x;
+  }
+}
